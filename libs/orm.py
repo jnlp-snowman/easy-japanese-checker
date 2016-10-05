0af59af47f75b5601de37d66dd024e59b428ed09
takahashi <takahashi@jnlp.org>
2016-10-05 22:31:06 +0900

    ormを追加

---
+++ libs/orm.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData, create_engine, orm
+from sqlalchemy import CHAR, VARCHAR, Integer, Column, PrimaryKeyConstraint, ForeignKey
+
+sql_DATABASE = 'mysql+pymysql://gakkai:gakkai@localhost/web_data?unix_socket=/mysql/mysql.sock&charset=utf8'
+sql_metadata = MetaData()
+sql_engine = create_engine(
+    sql_DATABASE,
+    encoding='utf-8',
+    pool_recycle=5,
+)
+
+db_session = orm.scoped_session(
+    orm.sessionmaker(autocommit=False,
+                    autoflush=False,
+                    bind=sql_engine)
+)
+
+sql_Base = declarative_base()
+sql_Base.query = db_session.query_property()
+
+class EasyUniDic(sql_Base):
+    __tablename__ = 'easy_unidic'
+
+    unidic_id = Column('unidic_id', VARCHAR(length=5), primary_key=True)
+    surface = Column('surface', VARCHAR(length=50))
+
+# merge
+# commit
+
+#
